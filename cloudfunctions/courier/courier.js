87db1528da4fa99e22ae3746151f9d731f97a46e
jie <zhouyang.me@gmail.com>
2020-05-19 12:11:41 +0800

    update: add delivery method

--- cloudfunctions/courier/courier.js
+++ cloudfunctions/courier/courier.js
@@ -4,21 +4,41 @@ const moment = require('moment')
 // const funcs = require('./utils/funcs')
 
 
+const deliveryMethod = {
+  takeaway: 'takeway',
+  delivery: 'delivery',
+  shipping: 'shipping'
+}
+
+const deliveryStatus = {
+  // 快递接单
+  accepted: 'accepted',
+  // 商户下单
+  created: 'created',
+  // 快递送达
+  deliveried: 'deliveried',
+  // 用户确认
+  received: 'received',
+
+}
 
 const getCourier = async (user) => {
   let result = null;
   try {
-    result = await db.collection('courier').where({user_id: user._id, is_enable: true}).limit(1).get()
-  } catch(e) {
+    result = await db.collection('courier').where({
+      user_id: user._id,
+      is_enable: true
+    }).limit(1).get()
+  } catch (e) {
     console.error(e)
   }
-  if(!result) {
+  if (!result) {
     return {
       status: false,
       message: 'fail_to_get_courier'
     }
   }
-  if(!result.data) {
+  if (!result.data) {
     return {
       status: true,
       data: null
@@ -39,11 +59,11 @@ const regist = async (event, wxContext, user) => {
   let courierResult = null
   try {
     courierResult = await getCourier(user)
-  } catch(e) {
+  } catch (e) {
     console.error(e)
   }
 
-  if(courierResult.status && courierResult.data) {
+  if (courierResult.status && courierResult.data) {
     return {
       status: false,
       message: 'courier_exists'
@@ -55,7 +75,7 @@ const regist = async (event, wxContext, user) => {
   courier.update_at = new Date()
   courier.is_enable = true
   courier.user_id = user._id
-  if(event.invite_by) {
+  if (event.invite_by) {
     courier.invite_by = event.invite_by
     courier.is_verified = true
   } else {
@@ -99,15 +119,15 @@ const updateCourier = async (event, wxContext, user) => {
   let courierResult = null
   try {
     courierResult = await getCourier(user)
-  } catch(e) {
+  } catch (e) {
     console.error(e)
   }
 
-  if(!courierResult.status) {
+  if (!courierResult.status) {
     return courierResult
   }
 
-  if(!courierResult.data) {
+  if (!courierResult.data) {
     return {
       status: false,
       message: 'courier_not_found'
@@ -115,7 +135,7 @@ const updateCourier = async (event, wxContext, user) => {
   }
 
   let courier = courierResult.data
-  if(courier.is_verified) {
+  if (courier.is_verified) {
     return {
       status: false,
       message: 'courier_verified'
@@ -136,9 +156,9 @@ const updateCourier = async (event, wxContext, user) => {
     console.error(e)
   }
 
-  if(!updateResult) {
+  if (!updateResult) {
     return {
-      status: false,                                                                                                              
+      status: false,
       message: 'fail_to_update_courier'
     }
   }
@@ -148,31 +168,214 @@ const updateCourier = async (event, wxContext, user) => {
   }
 }
 
-// 获取快递单详情
-const getOrderDetail  = async (event, wxContext, user) => {
+// 获取外送单详情
+const getDeliveryOrderDetail = async (event, wxContext, user) => {
   console.log('user:', user)
+  if (!event.order_id) {
+    return {
+      status: false,
+      message: 'order_id_required'
+    }
+  }
+
+  let result;
+  try {
+    result = db.collection('order').doc(event.order_id).get()
+  } catch (e) {
+    console.error(e)
+  }
+  if (!result) {
+    return {
+      status: false,
+      message: 'order_not_found'
+    }
+  }
+
+  if(!result.data.courier_id || !result.data.courier_id != user._id) {
+    return {
+      status: false,
+      message: 'order_not_found'
+    }
+  }
+
+  if (!result.data.delivery_method || result.data.delivery_method != deliveryMethod.delivery) {
+    return {
+      status: false,
+      message: 'delivery_method_error'
+    }
+  }
+
+  return {
+    orderInfo: result.data.orderInfo,
+    addressInfo: result.data.addressInfo
+  }
 }
 
 // 获取已接单列表
-const getOrders  = async (event, wxContext, user) => {
-  console.log('user:', user)
+const getDeliveryOrders = async (event, wxContext, user) => {
+  let result;
+  try {
+    result = db.collection('order').where({
+      courier_id: user._id,
+      is_enable: true
+    }).get()
+  } catch (e) {
+    console.error(e)
+  }
+  if (!result) {
+    return {
+      status: false,
+      message: 'fail_to_get_orders'
+    }
+  }
+
+  return {
+    status: true,
+    data: {
+      entities: result.data
+    }
+  }
+
 }
 
-// 更新快递单状态
-const updateOrder  = async (event, wxContext, user) => {
-  console.log('user:', user)
+// 更新外送单状态
+const updateDeliveryOrderInfo = async (event, wxContext, user) => {
+  if (!event.order_id) {
+    return {
+      status: false,
+      message: 'order_id_required'
+    }
+  }
+
+  let result = null
+  try {
+    result = db.collection('order').doc(event.order_id).get()
+  } catch (e) {
+    console.error(e)
+  }
+
+  if (!result) {
+    return {
+      status: false,
+      message: 'order_not_found'
+    }
+  }
+
+  if(result.data.delivery_method != deliveryMethod.delivery) {
+    return {
+      status: false,
+      message: 'delivery_method_error'
+    }
+  }
+
+  // 外送码校验错误
+  if(result.data.deliveryInfo.delivery_code != event.delivery_code) {
+    return {
+      status: false,
+      message: 'delivery_code_error'
+    }
+  }
+
+  let updateResult = null;
+  try {
+    updateResult = db.collection('order').where({
+      _id: event.order_id,
+      is_enable: true
+    }).set({
+      'deliveryInfo.status': event.delivery_status
+    })
+  } catch (e) {
+    console.error(e)
+  }
+
+  if (!updateResult) {
+    return {
+      status: false,
+      message: 'fail_to_update_delivery_status'
+    }
+  }
+
+  return {
+    status: true,
+    data: updateResult.data
+  }
 }
 
 // 快递员抢单
-const grabOrder = async (event, wxContext, user) => {
-  console.log('user:', user)
+const grabDeliveryOrder = async (event, wxContext, user) => {
+  if (!event.order_id) {
+    return {
+      status: false,
+      message: 'order_id_required'
+    }
+  }
+
+  let result = null
+  try {
+    result = db.collection('order').doc(event.order_id).get()
+  } catch (e) {
+    console.error(e)
+  }
+
+  if (!result) {
+    return {
+      status: false,
+      message: 'order_not_found'
+    }
+  }
+
+  if(!result.data.delivery_method || result.data.delivery_method != deliveryMethod.delivery) {
+    return {
+      status: false,
+      message: 'delivery_method_error'
+    }
+  }
+
+  if(result.data.deliveryInfo.status == deliveryStatus.accepted) {
+    return {
+      status: false,
+      message: 'order_grabed'
+    }
+  }
+
+  if(result.data.deliveryInfo.status != deliveryStatus.created) {
+    return {
+      status: false,
+      message: 'delivery_status_error'
+    }
+  }
+
+  let updateResult = null;
+  try {
+    updateResult = db.collection('order').where({
+      _id: event.order_id,
+      is_enable: true
+    }).set({
+      'deliveryInfo.status': deliveryStatus.accepted,
+      'courier_id': user._id
+    })
+  } catch (e) {
+    console.error(e)
+  }
+
+  if (!updateResult) {
+    return {
+      status: false,
+      message: 'fail_to_update_delivery_status'
+    }
+  }
+
+  return {
+    status: true,
+    data: updateResult.data
+  }
 }
 
 module.exports = {
   regist: regist,
   updateCourier: updateCourier,
-  getOrderDetail: getOrderDetail,
-  getOrders: getOrders,
-  updateOrder: updateOrder,
-  grabOrder: grabOrder
+  getDeliveryOrderDetail: getDeliveryOrderDetail,
+  getDeliveryOrders: getDeliveryOrders,
+  updateDeliveryOrderInfo: updateDeliveryOrderInfo,
+  grabDeliveryOrder: grabDeliveryOrder
 }
